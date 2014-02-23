c4600869bea99f270435c9870c8209e5eceb6928
筱乐[xiaole] <lianle.k@163.com>
2014-02-24 02:27:10 +0800

    Update Simulate interfaces with duck typing.js

--- Preprimary-Education/Jacascript-Interfaces/Simulate interfaces with duck typing.js
+++ Preprimary-Education/Jacascript-Interfaces/Simulate interfaces with duck typing.js
@@ -29,7 +29,8 @@ CompositeForm.prototype.save = function () {
 //...
 
 function addForm(formInstance) {
-    ensureImplements(formInstance, Composite, FormItem);
+    ensureImplements(formInstance, Composite, FormItem);//检查是否实现接口后再进行
+                                                        //接口中函数的调用
     // This function will throw an error if a required method is not implemented.
     //...
 }
