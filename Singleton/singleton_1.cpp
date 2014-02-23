4a678ec9bb8f151160b84ff11069864bab40368b
筱乐[xiaole] <lianle.k@163.com>
2014-02-23 11:27:47 +0800

    Create singleton_1.cpp

---
+++ Singleton/singleton_1.cpp
@@ -0,0 +1,22 @@
+#include "stdafx.h"
+#include "Singleton.h"
+
+#include <iostream>
+using namespace std;
+
+Singleton* Singleton::_instance = NULL;
+
+Singleton::Singleton()
+{
+	cout << "Singleton" << endl;
+}
+
+Singleton* Singleton::GetInstance()
+{
+	if (NULL == _instance)
+	{
+		_instance = new Singleton();
+	}
+
+	return _instance;
+}
