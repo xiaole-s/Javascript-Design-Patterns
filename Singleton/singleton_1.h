26b6017aed530d5cfdca1a43fd74727387193b93
筱乐[xiaole] <lianle.k@163.com>
2014-02-23 11:27:23 +0800

    Create singleton_1.h

---
+++ Singleton/singleton_1.h
@@ -0,0 +1,23 @@
+//Singleton 
+//1.
+//
+#ifndef SINGLETON_H
+#define SINGLETON_H
+
+#include <iostream>
+using namespace std;
+
+class Singleton
+{
+public:
+        static Singleton* GetInstance();
+
+protected:
+        Singleton();
+
+private:
+        static Singleton* _instance;
+};
+
+#endif
+//
