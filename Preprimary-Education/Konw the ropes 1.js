afb52b48cdecfbce0f43d96fc2a666227b92348f
筱乐[xiaole] <lianle.k@163.com>
2014-02-23 12:21:20 +0800

    Create Konw the ropes 1.js

---
+++ Preprimary-Education/Konw the ropes 1.js
@@ -0,0 +1,9 @@
+/* This version allows the calls to be chained. 
+   在对象方法中返回this使其可链式调用
+*/
+
+Function.prototype.method = function(name, fn) {
+    this.prototype[name] = fn;
+    return this;
+};
+
