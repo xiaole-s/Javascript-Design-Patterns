a98c7c6a700e254f3ad4758df2cc120b8a441a53
筱乐[xiaole] <lianle.k@163.com>
2014-03-24 18:32:04 +0800

    Update singleton_1.js

--- Singleton/singleton_1.js
+++ Singleton/singleton_1.js
@@ -1,18 +1,25 @@
-//use closures achieve the Singleton Design Pattern
-//Who is this Instance,we can wear the host's namespace
-var namespaceOfHost = {};
+//使用闭包实现cpp中Singleton代码的js实现
+var namespaceOfHost = {};//命名空间
 namespaceOfHost.getInstance = (function () {
 
-    var _instance = null;
-    function Singleton() {
+    var _instance = null;  //静态私有变量
+    function Singleton() { //Singleton类
         this.a = { x: 3, y: 4 };
         this.b = { x: 30, y: 40 };
         this.c = { x: 300, y: 40 };
     }
+    
+    Singleton.prototype = {
+        doSomething:function(p){},
+        d:3
+    }
 
     return function () {
         if (!_instance)
-            _instance = new Singleton();
+            _instance = new Singleton();//只创建一次实例
         return _instance;
     };
 })();
+
+//使用
+namespaceOfHost.getInstance.doSomething(x);
